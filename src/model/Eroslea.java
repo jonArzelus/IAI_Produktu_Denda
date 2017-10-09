b504e1153ddec9af13bf85df63829f10f1767c46
Julen Diez <julen.d.m@gmail.com>
2017-10-09 15:32:35 +0200

    Erosle klasea eginda

--- src/model/Eroslea.java
+++ src/model/Eroslea.java
@@ -2,4 +2,30 @@ package model;
 
 public class Eroslea {
 
+	String id, izena, pasahitza;
+	double dirua;
+	
+	public Eroslea(String i, String iz, String pwd, double dir) {
+		id = i;
+		izena = iz;
+		pasahitza = pwd;
+		dirua = dir;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getIzena() {
+		return izena;
+	}
+
+	public String getPasahitza() {
+		return pasahitza;
+	}
+
+	public double getDirua() {
+		return dirua;
+	}
+	
 }
